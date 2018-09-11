2288fc2934259fbc10c5959b7c50aba5dbaf0f00
RyanLe2000 <42581736+RyanLe2000@users.noreply.github.com>
2018-09-11 10:21:55 -0500

    Add files via upload

---
+++ 3.2 Practice.py
@@ -0,0 +1,9 @@
+age = (int(input('Select your age ')))
+if age <= 1:
+    print('Your an infant ')
+elif age < 13 and 2:
+ print('child ')
+elif age >= 13 and 20:
+    print('Teenager')
+else:
+    print('Adult ')
