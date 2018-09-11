6dff5aef113c31b23442371cfeb86f8dc865d9d2
RyanLe2000 <42581736+RyanLe2000@users.noreply.github.com>
2018-09-11 10:00:42 -0500

    Add files via upload

---
+++ 3.1 Practice.py
@@ -0,0 +1,17 @@
+number = (int(input('Press a Number ')))
+if 1 is number:
+    print('Monday')
+elif 2 is number:
+    print('Tuesday')
+elif 3 is number:
+    print('Wensday')
+elif 4 is number:
+    print('Thursday')
+elif 5 is number:
+    print('Friday')
+elif 6 is number:
+    print('Saturday')
+elif 7 is number:
+    print('Sunday')
+else:
+    print("Error has occured, please try again")
