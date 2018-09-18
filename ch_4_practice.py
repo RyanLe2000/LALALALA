3ab5dac8cc446ee0f54536f6feecc138d6a120e3
RyanLe2000 <42581736+RyanLe2000@users.noreply.github.com>
2018-09-18 09:59:43 -0500

    Add files via upload

--- ch_4_practice.py
+++ ch_4_practice.py
@@ -35,21 +35,44 @@ for x in range(11):
 # Have the user enter 5 numbers, provide a total at the end
 # You can assume integers
 
+user_total = 0
 
+for user_num in range (0, 5):
+    n_num = int(input('Enter a number '))
+    user_total += user_num
+    print(user_total)
 
 # TODO 4.5 Sentinel Value
 # Create a variable to store a total amount
 # Create a variable to store a count of the numbers entered
 # Have the user enter test scores until a sentinel value of -1 is
 # entered.
-# Display the total, the count and the average (total / count)
+# Display the total, the count and the a10
+# verage (total / count)
 
+tax = 0
+rec = 0
+soul = 0
+while soul != - 1:
+    soul = int(input('Enter the test scores, -1 to finish '))
+if soul != -1:
+    rec += 1
+    tax += soul
 
+    print('you put: ' + str(rec) + ' numbers')
+print('the total of all scores is: ' + str(tax))
+
+average = tax / rec
+print('the average: ' + format(average, ',.2f'))
 # TODO 4.6 validating data
 # Ask the user to enter a number between 1 and 10.
 # Use a while loop to force the user to repeat the
 # prompt until the user enters a valid number. Test with
 # both valid and invalid data
 
-go = int(input('1 to 10 '))
-while g >
+put = int(input('Enter a number between 1 - 10 '))
+while put > 5 or put < 5:
+    print('Invalid, please try again ')
+    put = int(input('Enter a number between 1 - 10 '))
+if put is 5:
+    print('you good')
