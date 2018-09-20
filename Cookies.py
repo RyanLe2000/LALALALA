4782226299342d3d1ba6abffa9099cc2f8cad46e
RyanLe2000 <42581736+RyanLe2000@users.noreply.github.com>
2018-09-20 10:15:22 -0500

    Update Cookies.py

--- Cookies.py
+++ Cookies.py
@@ -1,14 +1,20 @@
-cookie_num = int(input('How many cookies do you want to make? '))
+butter_total = 48 / 1
+sugar_total = 48 /1.5
+flour_total = 48 / 2.75
 
-butter_amt = float(input('How much butter do you have '))
-flour_amt = float(input('How much flour you have '))
-sugar_amt = float(input('How much sugar you have '))
+print('you can make 48 cookies with the following ingredients: ')
+print(format(butter_total, ',.2f') + " cups of butter")
+print(format(sugar_total, ',.2f') + " cups of sugar")
+print(format(flour_total, ',.2f') + " cups of flour")
 
-butter_total = butter_amt / cookie_num
-sugar_total = sugar_amt/ cookie_num
-flour_total = flour_amt/ cookie_num
+cookie = int(input('How many cookies do you want?'))
 
-print('you can make ' + str(cookie_num) + ' cookies with the following ingredients: ')
-print(str(butter_total) + ' sticks of butter')
-print(str(sugar_total) + ' ounces of sugar')
-print(str(flour_total) + ' pounds of flour')
+butter_total = cookie/1
+sugar_total = cookie/1.5
+flour_total = cookie/2.75
+
+print("you can make " , cookie , " cookies with the following")
+
+print(format(butter_total, ',.2f') + " cups of butter")
+print(format(sugar_total, ',.2f') + " cups of sugar")
+print(format(flour_total, ',.2f') + " cups of flour")
