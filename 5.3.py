1e25262fd6d92df261a7078009e2ccc943c7c6f9
RyanLe2000 <42581736+RyanLe2000@users.noreply.github.com>
2018-09-25 09:56:40 -0500

    Update 5.3.py

--- 5.3.py
+++ 5.3.py
@@ -1,5 +1,28 @@
 def main():
-    get input(   )
+    get input(score1 + score2 + score3 + score4 + score5  )
     ave = calc_average(+1, +2, +3, +4, +5)
     det_grade(ave)
     calc_average (51, 52, 53, 54, 55)
+    print(" your first score is ", score1)
+    print(" your second score is ", score2)
+    print(" your third score is ", score3)
+    print(" your forth score is ", score4)
+    print(" your fidth score is ", score5)
+    print("your average score is ", ave)
+    def score1():
+        score1 = int(input(" whats the first test score?))
+        return score1
+    def score2():
+        score2 = int(input(" whats the second test score?))
+        return score2
+    def score3():
+        score3 = int(input(" whats the third test score?))
+        return score3                 
+    def score4():    
+        score4 = int(input(" whats the forth test score?))
+        return score4                  
+    def score5():
+        score5 = int(input(" whats the fidth test score?))
+        return score5                   
+main()                     
+                           
