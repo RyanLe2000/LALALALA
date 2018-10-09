cb4ae9c2b14f655b1b03ac698b532f04d1ed18d4
RyanLe2000 <42581736+RyanLe2000@users.noreply.github.com>
2018-10-09 10:29:26 -0500

    Add files via upload

--- 7.3.py
+++ 7.3.py
@@ -2,6 +2,53 @@
 #The user should be able to enter a boy's name or a girl's name. The application should check both lists,
 #and then display messages indicating whether the names were among the most popular
 #if the name was on one of the lists or that the name was not on the lists of popular names.
+def main():
+    boy_list = []
+    try:
+        boy_name = open('BoyNames.txt', 'r')
 
-boy = open('BoyNames.txt', 'r')
-girl = open('Girlnames.txt', 'r')
+        for boy in boy_name:
+            boy = boy.rstrip('\n')
+            boy_list.append
+
+        print(boy_list)
+        boy_name.close()
+
+        user_name = input(' enter name boy ')
+
+        if user_name is boy_list:
+            print(' thats a pop name')
+        else:
+            print(' that name doesnt exist ')
+
+    except IOError:
+        print('sorry that name isnt here')
+
+
+main()
+
+
+def main():
+    girl_list = []
+    try:
+        girl_name = open('GirlNames.txt', 'r')
+
+        for girl in girl_name:
+            girl = girl.rstrip('\n')
+            girl_list.append
+
+        print(girl_list)
+        girl_name.close()
+
+        user_name = input(' enter name girl ')
+
+        if user_name is girl_list:
+            print(' thats a pop name')
+        else:
+            print(' that name doesnt exist ')
+
+    except IOError:
+        print('sorry that name isnt here')
+
+
+main()
