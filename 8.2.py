88338bf0e978e22f34d4088595eb51e4588ccd5e
RyanLe2000 <42581736+RyanLe2000@users.noreply.github.com>
2018-10-16 15:46:51 -0500

    Add files via upload

--- 8.2.py
+++ 8.2.py
@@ -1,9 +1,18 @@
 def main():
-    alpha = ['0', '1', '2', 'A', 'B', 'C', '3', 'D', 'E', 'F', '4', 'G', 'H', 'I', '5', 'J', 'K', 'L', '6', 'M', 'N',
+    digits = ['0', '1', '2', 'A', 'B', 'C', '3', 'D', 'E', 'F', '4', 'G', 'H', 'I', '5', 'J', 'K', 'L', '6', 'M', 'N',
          'O', '7', 'P', 'Q', 'R', 'S', '8', 'T', 'U', 'V', '9', 'W', 'X', 'Y', 'Z', '-']
 
-    digits = ['0', '1', '2', '2', '2', '2', '3', '3', '3', '3', '4', '4', '4', '4', '5', '5', '5', '5',
+    alpha = ['0', '1', '2', '2', '2', '2', '3', '3', '3', '3', '4', '4', '4', '4', '5', '5', '5', '5',
           '6', '6', '6', '6', '7', '7', '7', '7', '7', '8', '8', '8', '8', '9', '9', '9', '9', '9', '-']
 
-    phrase = input(' input word to be chiphered')
+    phrase = input(' input word to be chiphered ')
+    print(len(phrase))
+    alpha_final = ""
+    for letter in phrase:
+        for item in range(0, len(digits)):
+            if letter.upper() == digits[item]:
+                alpha_final += (alpha[item] + " ")
 
+
+    print(alpha_final)
+main()
