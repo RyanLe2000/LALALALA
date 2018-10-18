caa329f8f1cde6fc9aed72a298cc6d225467768c
RyanLe2000 <42581736+RyanLe2000@users.noreply.github.com>
2018-10-18 10:30:14 -0500

    Add files via upload

---
+++ 9,1.py
@@ -0,0 +1,18 @@
+
+
+
+
+
+Lesson Objective(s):
+Write a CRUD - Create, Read, Update, Delete application
+Pickle files and save them
+Unpickle and save into data structures
+
+
+Lesson:
+Write a program that keeps names and email addresses in a dictionary as key-value pairs. The program should display a
+menu that lets the user look up a person's email address, add a new name and email address, change an existing email '
+    'address, and delete an existing name and email address. The program should '
+  'pickle the dictionary and save it to a file when the user exits the program.'
+  ' Each time the program starts, it should retrieve the dictionary from the file '
+   'and unpickle it.
