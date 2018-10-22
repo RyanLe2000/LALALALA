19367ec233cea842ed51a73a382206c4e049914e
RyanLe2000 <42581736+RyanLe2000@users.noreply.github.com>
2018-10-22 10:28:25 -0500

    Update 9,1.py

--- 9,1.py
+++ 9,1.py
@@ -16,3 +16,46 @@ menu that lets the user look up a person's email address, add a new name and ema
   'pickle the dictionary and save it to a file when the user exits the program.'
   ' Each time the program starts, it should retrieve the dictionary from the file '
    'and unpickle it.
+import pickle
+def main():# declare variables
+    customers = get_info()
+    choice = menu()
+    if choice == "look":
+        lookup(customers)
+    elif choice == "add":
+        add(customers)
+    elif choice == "update":
+        change(customers)
+    elif choice == "remove":
+        delete(customers)
+    elif choice == "quit":
+        save_file(customers)
+        else:print("not valid entry")
+            menu()
+            
+            
+def get_info():
+    # get file, unpickle, store in dictionarytry:
+        customer_file = open('customers.dat', 'rb')
+        customers_dictionary = pickle.load(customer_file)
+    except FileNotFoundError:
+        customer_file = open('customers.dat', 'wb')
+        customers_dictionary = {}
+        customer_file.close()
+    return customers_dictionary
+
+
+def menu():
+    # display menu
+    chosen = 0
+    try:
+        while chosen < 1 or chosen >2:
+        print("----------- Customer Accounts --------------")
+        print("1:   Add a new customer")
+        print("2:   Quit")
+        chosen = int(input("Please enter the number of your selection:"))
+        if chosen < 1 or chosen > 2:
+            print("Not a valid selection.")
+        else:if chosen == 1:
+                return "add"
+            elif chosen == 2:return "quit"except ValueError:print("You need to have a numeric entry from the above list")menu()def lookup(customers_lookup):# lookup and display a current customerprint("lookup")def add(customers_add):# add a new customerprint("add")def change(customers_change):# update customer informationprint("change")def delete(customers_delete):# delete a customerprint("delete")def save_file(customers_pickle):# pickle and dump the fileprint("save")main()
